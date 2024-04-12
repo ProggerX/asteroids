d9326e9201365da5fb5dec4fb3636ad6b97ca810
ProggerX <x@proggers.ru>
2024-04-12 15:55:48 +0300

    Added normal res

--- main.go
+++ main.go
@@ -62,7 +62,7 @@ func asteroidLoop(ast *entities.Entity, _ event.EnterPayload) event.Response {
 	ast.Delta[1] = float64(2 * (diff + 150) / 150)
 	if collision.HitLabel(ast.Space, Bullet) != nil {
 		go func() {
-			spawn := floatgeom.Point2{float64(random.Intn(550)), float64(random.Intn(50) - 150)}
+			spawn := floatgeom.Point2{float64(random.Intn(600)), float64(random.Intn(50) - 150)}
 			time.Sleep(time.Millisecond * 20);
 			ast.SetPos(spawn)
 			int_score, _ := strconv.Atoi(score)
@@ -71,9 +71,9 @@ func asteroidLoop(ast *entities.Entity, _ event.EnterPayload) event.Response {
 			diff++
 		}()
 	}
-	if ast.Y() > 500 {
+	if ast.Y() > 360 {
 		go func() {
-			spawn := floatgeom.Point2{float64(random.Intn(550)), float64(random.Intn(50) - 150)}
+			spawn := floatgeom.Point2{float64(random.Intn(600)), float64(random.Intn(50) - 150)}
 			ast.SetPos(spawn)
 			int_lives, _ := strconv.Atoi(lives)
 			int_lives--
@@ -96,14 +96,15 @@ func getImageFromFilePath(filePath string) image.Image {
 }
 
 func startScene(ctx *scene.Context) {
+	oak.UpdateViewSize(640, 360)
 	globalCtx = ctx
 	ctx.Draw(render.NewStrPtrText(&score, 10, 10))
 	ctx.Draw(render.NewStrPtrText(&lives, 10, 25))
-	player := entities.New(ctx, entities.WithColor(color.White), entities.WithRect(floatgeom.NewRect2WH(0, 0, 20, 20)), entities.WithPosition(floatgeom.Point2{250, 460}))
+	player := entities.New(ctx, entities.WithColor(color.White), entities.WithRect(floatgeom.NewRect2WH(0, 0, 20, 20)), entities.WithPosition(floatgeom.Point2{250, 340}))
 	event.Bind(ctx, event.Enter, player, playerLoop)
 	event.Bind(ctx, key.Down(key.Spacebar), player, shoot)
 	for range 5 {
-		ast := entities.New(ctx, entities.WithLabel(Asteroid), entities.WithRect(floatgeom.NewRect2WH(0, 0, 64, 64)), entities.WithColor(color.White), entities.WithPosition(floatgeom.Point2{float64(random.Intn(550)), float64(random.Intn(50) - 150)}))
+		ast := entities.New(ctx, entities.WithLabel(Asteroid), entities.WithRect(floatgeom.NewRect2WH(0, 0, 64, 64)), entities.WithColor(color.White), entities.WithPosition(floatgeom.Point2{float64(random.Intn(600)), float64(random.Intn(50) - 150)}))
 		ast.Delta[1] = 2
 		event.Bind(ctx, event.Enter, ast, asteroidLoop)
 	}
